2db178f56f8e6380bb375812a726048e9546ebc0
PReichetanz <paul.reichetanz@gmx.de>
2021-11-22 14:22:31 +0100

    Add basic form with styling

--- src/app/components/Form/Form.tsx
+++ src/app/components/Form/Form.tsx
@@ -1,5 +1,37 @@
 import React from 'react';
+import styled from 'styled-components';
 
 export default function Form(): JSX.Element {
-  return <>Test</>;
+  return (
+    <FormContainer>
+      <label htmlFor="name">Name des Schülers:</label>
+      <input type="text" id="name" placeholder="Lena Beispiel" required />
+      <label htmlFor="evaluation">Worturteil:</label>
+      <textarea
+        id="evaluation"
+        rows={3}
+        placeholder="Lena arbeitet häufig gut mit."
+        required
+      />
+      <SubmitButton type="submit">Hinzufügen</SubmitButton>
+    </FormContainer>
+  );
 }
+
+const FormContainer = styled.form`
+  background: var(--color-background-light);
+  color: var(--color-text-dark);
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+  padding: 0.5rem;
+  font-weight: 700;
+`;
+
+const SubmitButton = styled.button`
+  background: var(--color-button);
+  border-radius: 0.5rem;
+  border: 1px solid var(--color-stroke);
+  font-weight: 700;
+  padding: 0.5rem;
+`;
