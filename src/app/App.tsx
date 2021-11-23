d9be7c768ef55787d8a2277cb78b729a22500620
PReichetanz <paul.reichetanz@gmx.de>
2021-11-23 15:38:28 +0100

    Add form component to App.tsx with state of pupils

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Form from './components/Form/Form';
+
+type Pupil = {
+  name: string;
+  evaluation: string;
+};
 
 function App(): JSX.Element {
-  return <>Hello world!</>;
+  const [pupils, setPupils] = useState<Pupil[]>([]);
+  console.log(pupils);
+
+  function handleFormSubmit(pupil: { name: string; evaluation: string }) {
+    setPupils([
+      ...pupils,
+      {
+        name: pupil.name,
+        evaluation: pupil.evaluation,
+      },
+    ]);
+  }
+
+  return (
+    <>
+      <Form
+        nameLabel="Name des Schülers"
+        evaluationLabel="Worturteil"
+        onSubmit={handleFormSubmit}
+        submitted={false}
+      />
+    </>
+  );
 }
 
 export default App;
