8bcac56b878cc0d3f552233b720003d91a640cc0
PReichetanz <paul.reichetanz@gmx.de>
2021-11-24 10:53:59 +0100

    Add content of controlled Card with one evaluation

--- src/app/components/Card/Card.stories.tsx
+++ src/app/components/Card/Card.stories.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import Card from './Card';
 
+const testPupil = {
+  name: 'Lena Beispiel',
+  evaluation:
+    'Lena ist immer gut vorbereitet und arbeitet häufig aufmerksam mit.',
+};
+
 export default {
   title: 'Component/Card',
   component: Card,
 };
 
-export const regular = (): JSX.Element => <Card />;
+export const regular = (): JSX.Element => <Card pupil={testPupil} />;
--- src/app/components/Card/Card.tsx
+++ src/app/components/Card/Card.tsx
@@ -1,5 +1,38 @@
 import React from 'react';
+import styled from 'styled-components';
 
-export default function Card(): JSX.Element {
-  return <article>Hello world!</article>;
+type CardProps = {
+  pupil: {
+    name: string;
+    evaluation: string;
+  };
+};
+
+export default function Card({ pupil }: CardProps): JSX.Element {
+  const { name, evaluation } = pupil;
+  return (
+    <Container>
+      <Heading>{name}</Heading>
+      <Evaluation>{evaluation}</Evaluation>
+    </Container>
+  );
 }
+
+const Container = styled.article`
+  background: var(--color-background-dark);
+  border-radius: 0.5rem;
+  width: 90%;
+  margin: auto;
+`;
+
+const Heading = styled.h1`
+  color: var(--color-text-white);
+  padding: 0.5rem 0.75rem;
+  margin: 0;
+`;
+
+const Evaluation = styled.p`
+  color: var(--color-text-lightgreen);
+  padding: 0.5rem 1rem;
+  margin: 0;
+`;
