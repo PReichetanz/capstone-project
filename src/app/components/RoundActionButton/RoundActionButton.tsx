31498175e56df1cb3559b691dc7a3217fbe8fd6f
PReichetanz <paul.reichetanz@gmx.de>
2021-11-25 08:49:59 +0100

    Refactor button
    - Make style and positioning controllable
    - Rename ButtonProps

--- src/app/components/RoundActionButton/RoundActionButton.tsx
+++ src/app/components/RoundActionButton/RoundActionButton.tsx
@@ -2,41 +2,41 @@ import React from 'react';
 import type { ReactNode } from 'react';
 import styled from 'styled-components';
 
-type DeleteButtonProps = {
+type RoundActionButtonProps = {
   handleDelete: () => void;
   children: ReactNode;
 };
 
-export default function DeleteButton({
+export default function RoundActionButton({
   handleDelete,
   children,
-}: DeleteButtonProps): JSX.Element {
-  return (
-    <Container>
-      <Button onClick={handleDelete}>{children}</Button>
-    </Container>
-  );
+}: RoundActionButtonProps): JSX.Element {
+  return <Button onClick={handleDelete}>{children}</Button>;
 }
 
-const Container = styled.div`
+interface StylingProps {
+  sizeButton?: string;
+  topPosition?: string;
+  rightPosition?: string;
+}
+
+const Button = styled.button.attrs<StylingProps>((props) => ({
+  sizeButton: props.sizeButton || '2rem',
+  topPosition: props.topPosition || '0.25rem',
+  rightPosition: props.rightPosition || '0.25rem',
+}))`
+  position: absolute;
+  right: ${(props: StylingProps) => props.rightPosition};
+  top: ${(props: StylingProps) => props.topPosition};
   display: flex;
   justify-content: center;
   align-items: center;
+  width: ${(props: StylingProps) => props.sizeButton};
+  height: ${(props: StylingProps) => props.sizeButton};
   background: var(--color-tertiary);
   border-radius: 50%;
   border: 1px solid var(--color-stroke);
   padding: 0.625rem;
-  position: absolute;
-  right: 0.25rem;
-  top: 0.25rem;
-  &:hover {
-    background: var(--color-tertiary-hover);
-  }
-`;
-
-const Button = styled.button`
-  border: none;
-  background: inherit;
   color: var(--color-buttonText);
   font-weight: 700;
 `;
