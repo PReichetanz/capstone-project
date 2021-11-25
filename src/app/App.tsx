30f112f478176a23a7517554ab63df78b6759f64
PReichetanz <paul.reichetanz@gmx.de>
2021-11-25 08:00:02 +0100

    Add validation of  existingPupil

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -11,14 +11,39 @@ function App(): JSX.Element {
   const [pupils, setPupils] = useState<Pupil[]>([]);
   console.log(pupils);
 
+  function findPupil(pupil: { name: string; evaluation: string }) {
+    const newPupil = pupil;
+    const existingPupil = pupils.find((pupil) => pupil.name === newPupil.name);
+    if (existingPupil === undefined) {
+      return false;
+    } else {
+      console.log(existingPupil);
+      return existingPupil;
+    }
+  }
+
   function handleFormSubmit(pupil: { name: string; evaluation: string }) {
-    setPupils([
-      ...pupils,
-      {
-        name: pupil.name,
-        evaluations: [pupil.evaluation],
-      },
-    ]);
+    const existingPupil = findPupil(pupil);
+    if (existingPupil) {
+      const existingPupilId = pupils.findIndex(
+        (pupil) => pupil.name === existingPupil.name
+      );
+      existingPupil.evaluations = [
+        ...existingPupil.evaluations,
+        pupil.evaluation,
+      ];
+      const newPupils = pupils.slice();
+      newPupils[existingPupilId] = existingPupil;
+      setPupils(newPupils);
+    } else {
+      setPupils([
+        ...pupils,
+        {
+          name: pupil.name,
+          evaluations: [pupil.evaluation],
+        },
+      ]);
+    }
   }
 
   function deletePupil(name: string) {
