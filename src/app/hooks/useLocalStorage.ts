06940127a6e13f9c4cb3765024a282841457b6bb
PReichetanz <paul.reichetanz@gmx.de>
2021-11-26 14:06:55 +0100

    Refactor useLocalStorage
    - Add try and catch as suggested in review

--- src/app/hooks/useLocalStorage.ts
+++ src/app/hooks/useLocalStorage.ts
@@ -3,15 +3,24 @@ import { useState } from 'react';
 export default function useLocalStorage<T>(
   key: string,
   initialValue: T
-): [storedValue: T, setValue: (value: T) => void] {
-  const [storedValue, setStoredValue] = useState<T>(() => {
-    const item = localStorage.getItem(key);
-    return item ? JSON.parse(item) : initialValue;
+): [T, (value: T) => void] {
+  const [storedValue, setStoredValue] = useState(() => {
+    try {
+      const item = localStorage.getItem(key);
+      return item ? JSON.parse(item) : initialValue;
+    } catch (error) {
+      console.log(error);
+      return initialValue;
+    }
   });
 
   const setValue = (value: T) => {
-    setStoredValue(value);
-    localStorage.setItem(key, JSON.stringify(value));
+    try {
+      setStoredValue(value);
+      localStorage.setItem(key, JSON.stringify(value));
+    } catch (error) {
+      console.log(error);
+    }
   };
   return [storedValue, setValue];
 }
