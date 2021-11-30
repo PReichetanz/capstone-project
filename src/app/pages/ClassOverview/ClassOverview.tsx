c490016c3b5553cf59f44a7880a4507204eed4aa
PReichetanz <paul.reichetanz@gmx.de>
2021-11-30 13:12:44 +0100

    Refactor ClassOverview (lifting state up)
    - Lift state up to App.tsx
    - Add props type and adjust function

--- src/app/pages/ClassOverview/ClassOverview.tsx
+++ src/app/pages/ClassOverview/ClassOverview.tsx
@@ -1,57 +1,32 @@
-import React, { useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 import Button from '../../components/Button/Button';
 import Card from '../../components/Card/Card';
 import Form from '../../components/Form/Form';
 import Header from '../../components/Header/Header';
-import useLocalStorage from '../../hooks/useLocalStorage';
 
 type Pupil = {
   name: string;
   evaluations: string[];
 };
 
-export default function ClassOverview(): JSX.Element {
-  const [pupils, setPupils] = useLocalStorage<Pupil[]>('myPupils', []);
-  const [isFormShown, setIsFormShown] = useState(false);
+type ClassOverviewProps = {
+  isFormShown: boolean;
+  setIsFormShown: (value: boolean) => void;
+  handleFormSubmit: (pupil: { name: string; evaluation: string }) => void;
+  pupils: Pupil[];
+  deletePupil: (name: string) => void;
+};
+
+export default function ClassOverview({
+  isFormShown,
+  setIsFormShown,
+  handleFormSubmit,
+  pupils,
+  deletePupil,
+}: ClassOverviewProps): JSX.Element {
   const navigate = useNavigate();
-
-  function findPupilByName(name: string) {
-    return pupils.find((pupil) => pupil.name === name);
-  }
-
-  function handleFormSubmit(pupil: { name: string; evaluation: string }) {
-    const existingPupil = findPupilByName(pupil.name);
-    if (existingPupil) {
-      const existingPupilId = pupils.findIndex(
-        (pupil) => pupil.name === existingPupil.name
-      );
-      existingPupil.evaluations = [
-        ...existingPupil.evaluations,
-        pupil.evaluation,
-      ];
-      const newPupils = pupils.slice();
-      newPupils[existingPupilId] = existingPupil;
-      setPupils(newPupils);
-      setIsFormShown(false);
-    } else {
-      setPupils([
-        ...pupils,
-        {
-          name: pupil.name,
-          evaluations: [pupil.evaluation],
-        },
-      ]);
-      setIsFormShown(false);
-    }
-  }
-
-  function deletePupil(name: string) {
-    const newPupilsList = pupils.filter((pupil) => pupil.name !== name);
-    setPupils(newPupilsList);
-  }
-
   return (
     <Container>
       <Header>Meine Klasse</Header>
