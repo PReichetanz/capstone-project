f0f592369c28e74370acde9fb2695bf767daaa81
PReichetanz <paul.reichetanz@gmx.de>
2021-12-01 09:00:47 +0100

    Refactor App.tsx
    - Move all content to pages
    - Only routing left

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,102 +1,13 @@
-import React, { useState } from 'react';
-import { Routes, Route, useParams, useNavigate } from 'react-router-dom';
-import useLocalStorage from './hooks/useLocalStorage';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import ClassOverview from './pages/ClassOverview/ClassOverview';
 import PupilOverview from './pages/PupilOverview/PupilOverview';
 
-type Pupil = {
-  name: string;
-  evaluations: string[];
-};
-
 export default function App(): JSX.Element {
-  const [pupils, setPupils] = useLocalStorage<Pupil[]>('myPupils', []);
-  const [isFormShown, setIsFormShown] = useState(false);
-  const navigate = useNavigate();
-
-  function findPupilByName(name: string) {
-    return pupils.find((pupil) => pupil.name === name);
-  }
-
-  function handleFormSubmit(pupil: { name: string; evaluation: string }) {
-    const existingPupil = findPupilByName(pupil.name);
-    if (existingPupil) {
-      const existingPupilId = pupils.findIndex(
-        (pupil) => pupil.name === existingPupil.name
-      );
-      existingPupil.evaluations = [
-        ...existingPupil.evaluations,
-        pupil.evaluation,
-      ];
-      const newPupils = pupils.slice();
-      newPupils[existingPupilId] = existingPupil;
-      setPupils(newPupils);
-      setIsFormShown(false);
-    } else {
-      setPupils([
-        ...pupils,
-        {
-          name: pupil.name,
-          evaluations: [pupil.evaluation],
-        },
-      ]);
-      setIsFormShown(false);
-    }
-  }
-
-  function deletePupil(name: string) {
-    const newPupilsList = pupils.filter((pupil) => pupil.name !== name);
-    setPupils(newPupilsList);
-  }
-
-  function deleteEvaluation(name: string, evaluationToDelete: string) {
-    const existingPupil = findPupilByName(name);
-    if (existingPupil) {
-      const existingPupilId = pupils.findIndex(
-        (pupil) => pupil.name === existingPupil.name
-      );
-
-      const newEvaluationsList = existingPupil.evaluations.filter(
-        (evaluation: string) => evaluation !== evaluationToDelete
-      );
-      if (newEvaluationsList.length === 0) {
-        deletePupil(name);
-        navigate('/');
-        return;
-      } else {
-        existingPupil.evaluations = newEvaluationsList;
-        const newPupils = pupils.slice();
-        newPupils[existingPupilId] = existingPupil;
-        setPupils(newPupils);
-      }
-    }
-  }
-
-  function PupilHandler({ pupils }: { pupils: Pupil[] }): JSX.Element {
-    const params = useParams();
-    const pupil = pupils.find((pupil) => pupil.name === params.pupilName);
-    return pupil ? (
-      <PupilOverview pupil={pupil} deleteEvaluation={deleteEvaluation} />
-    ) : (
-      <h1>404 Page not found</h1>
-    );
-  }
-
   return (
     <Routes>
-      <Route
-        path="/"
-        element={
-          <ClassOverview
-            isFormShown={isFormShown}
-            setIsFormShown={setIsFormShown}
-            handleFormSubmit={handleFormSubmit}
-            pupils={pupils}
-            deletePupil={deletePupil}
-          />
-        }
-      />
-      <Route path="/pupil" element={<PupilHandler pupils={pupils} />}>
+      <Route path="/" element={<ClassOverview />} />
+      <Route path="/pupil" element={<PupilOverview />}>
         <Route path=":pupilName" />
       </Route>
     </Routes>
