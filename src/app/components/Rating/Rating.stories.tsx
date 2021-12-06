6d003b7f5d0f9e45aad4de09c0146a24ad028762
PReichetanz <paul.reichetanz@gmx.de>
2021-12-06 12:38:34 +0100

    Delete Rating component for  react-simple-star

--- src/app/components/Rating/Rating.stories.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Rating from './Rating';
-
-export default {
-  title: 'Component/Rating',
-  component: Rating,
-};
-
-export const regular = (): JSX.Element => <Rating />;
