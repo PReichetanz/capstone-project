ff163911cd0d2e96569ce92868b997c1560c980a
PReichetanz <paul.reichetanz@gmx.de>
2022-05-21 23:15:00 +0200

    Add Rating component

---
+++ src/app/components/Rating/Rating.stories.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Rating from './Rating';
+
+export default {
+  title: 'Component/Rating',
+  component: Rating,
+};
+
+export const Regular = (): JSX.Element => (
+  <Rating ></Rating>
+);
